Purr4me/TES5Edit-GoogleCode

commit d1673743d62f98277059a2baa5c26ebf6492cd65

[ALL] Correct issue 125: SetToDefault will allocate entries full of zeros if aBasePtr is nil on entry. Also synced to r1257 and r1258.
Renamed Iwb6of8EnumDef to IwbKey2Data6EnumDef because I also need IwbData6Key2EnumDef for TES5saves.
Updated one script in XE3. Also corrected the second copy of vtEditors.pas
Misc: Removed empty line.


git-svn-id: http://skyrim-plugin-decoding-project.googlecode.com/svn@1259 bdfc8fb3-84cf-a68a-e902-ed0415cf5604

TES5Edit/branches/dev-experimental/wbInterface.pas
@@ -858,9 +858,10 @@ type
 
     function MasterRecordsFromMasterFilesAndSelf: TDynMainRecords;
 
-    {>>> Form Version access  <<<}
     function GetFormVersion: Cardinal;
     procedure SetFormVersion(aFormVersion: Cardinal);
+
+    procedure ChangeFormSignature(aSignature: TwbSignature);
 
     property Version: Cardinal
       read GetFormVersion
@@ -1354,7 +1355,7 @@ type
       read GetNameCount;
   end;
 
-  Iwb6of8EnumDef = interface(IwbEnumDef)
+  IwbKey2Data6EnumDef = interface(IwbEnumDef)
     ['{A3AFE02E-F72D-4E0E-BC56-219F7EE2B565}']
   end;
 
@@ -2037,8 +2038,8 @@ function wbEmpty(const aName      : string;
 
 function wbDumpInteger : IwbIntegerDefFormater; overload;
 
-function wb6of8Enum(const aNames : array of string)
-                                 : Iwb6of8EnumDef; overload;
+function wbKey2Data6Enum(const aNames : array of string)
+                                      : IwbKey2Data6EnumDef; overload;
 
 function wbFormID: IwbFormID; overload;
 
@@ -3028,6 +3029,7 @@ type
     function GetIsEditable(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
     function GetEditType(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): TwbEditType; override;
     function GetEditInfo(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): string; override;
+    function SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
 
     {---IwbUnionDef---}
     function Decide(aBasePtr: Pointer; aEndPtr: Pointer; const aElement: IwbElement): IwbValueDef;
@@ -3080,6 +3082,7 @@ type
     function ToNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Variant; override;
     procedure FromNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement; const aValue: Variant); override;
     function GetIsEditable(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
+    function SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
 
     {---IwbStringDef---}
     function GetStringSize: Integer;
@@ -3154,6 +3157,7 @@ type
     function ToNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Variant; override;
     procedure FromNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement; const aValue: Variant); override;
     function GetIsEditable(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
+    function SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
   end;
 
   TwbByteArrayDef = class(TwbValueDef, IwbByteArrayDef)
@@ -3207,6 +3211,7 @@ type
     function ToNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Variant; override;
     procedure FromNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement; const aValue: Variant); override;
     function GetIsEditable(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
+    function SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
   end;
 
   TwbEmptyDef = class(TwbValueDef, IwbEmptyDef)
@@ -3621,7 +3626,7 @@ type
     function GetNameCount: Integer;
   end;
 
-  Twb6of8EnumDef = class(TwbEnumDef, Iwb6of8EnumDef)
+  TwbKey2Data6EnumDef = class(TwbEnumDef, IwbKey2Data6EnumDef)
   protected
     {---IwbIntegerDefFormater---}
     function ToString(aInt: Int64; const aElement: IwbElement): string; override;
@@ -4517,9 +4522,9 @@ begin
   Result := TwbDumpIntegerDefFormater.Create(cpNormal, False);
 end;
 
-function wb6of8Enum(const aNames : array of string) : Iwb6of8EnumDef;
+function wbKey2Data6Enum(const aNames : array of string) : IwbKey2Data6EnumDef;
 begin
-  Result := Twb6of8EnumDef.Create(aNames, []);
+  Result := TwbKey2Data6EnumDef.Create(aNames, []);
 end;
 
 var
@@ -6240,7 +6245,7 @@ end;
 
 function TwbIntegerDef.SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean;
 begin
-  Result := ToInt(aBasePtr, aEndPtr, aElement) <> inDefault;
+  Result := not Assigned(aBasePtr) or (ToInt(aBasePtr, aEndPtr, aElement) <> inDefault);
   if Result then
     FromInt(inDefault, aBasePtr, aEndPtr, aElement);
 end;
@@ -7578,6 +7583,14 @@ begin
   Result := sdSize;
 end;
 
+function TwbStringDef.SetToDefault(aBasePtr, aEndPtr: Pointer;
+  const aElement: IwbElement): Boolean;
+begin
+  Result := not Assigned(aBasePtr) or (ToString(aBasePtr, aEndPtr, aElement) <> '');
+  if Result then
+    FromEditValue(aBasePtr, aEndPtr, aElement, '');
+end;
+
 function TwbStringDef.GetSize(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Integer;
 begin
   {if not Assigned(aBasePtr) or (Cardinal(aBasePtr) >= Cardinal(aEndPtr)) then
@@ -7745,7 +7758,7 @@ var
   Value: Extended;
 begin
   Value := ToNativeValue(aBasePtr, aEndPtr, aElement);
-  Result := not SingleSameValue(Value, fdDefault);
+  Result := not Assigned(aBasePtr) or not SingleSameValue(Value, fdDefault);
   if Result then
     FromNativeValue(aBasePtr, aEndPtr, aElement, fdDefault);
 end;
@@ -8953,6 +8966,23 @@ begin
   defReported := True;
 end;
 
+function TwbByteArrayDef.SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean;
+var
+  Size : Integer;
+  Default : String;
+begin
+  Size := GetSize(aBasePtr, aEndPtr, aElement);
+  if Size > 0  then begin
+    Default := '00';
+    while Length(Default)<(Size*3-1) do
+      Default := Default + ' 00';
+  end else
+    Default := '';
+  Result := not Assigned(aBasePtr) or (ToString(aBasePtr, aEndPtr, aElement) <> Default);
+  if Result then
+    FromEditValue(aBasePtr, aEndPtr, aElement, Default);
+end;
+
 function TwbByteArrayDef.ToEditValue(aBasePtr, aEndPtr: Pointer;
   const aElement: IwbElement): string;
 begin
@@ -9997,6 +10027,11 @@ begin
   defReported := True;
 end;
 
+function TwbUnionDef.SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean;
+begin
+  Result := Decide(aBasePtr, aEndPtr, aElement).SetToDefault(aBasePtr, aEndPtr, aElement);
+end;
+
 function TwbUnionDef.ToEditValue(aBasePtr, aEndPtr: Pointer;
   const aElement: IwbElement): string;
 begin
@@ -10337,6 +10372,14 @@ begin
       Result := Len;
   end else
     Result := Prefix;
+end;
+
+function TwbLenStringDef.SetToDefault(aBasePtr, aEndPtr: Pointer;
+  const aElement: IwbElement): Boolean;
+begin
+  Result := not Assigned(aBasePtr) or (ToString(aBasePtr, aEndPtr, aElement) <> '');
+  if Result then
+    FromEditValue(aBasePtr, aEndPtr, aElement, '');
 end;
 
 function TwbLenStringDef.ToEditValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): string;
@@ -10773,17 +10816,17 @@ end;
 
 function TwbDumpIntegerDefFormater.ToString(aInt: Int64; const aElement: IwbElement): string;
 begin
-  Result := IntToStr(aInt) + ' [' + IntToHex64(aInt, 8) + ']';
+  Result := IntToStr(aInt) + ' [' + IntToHex64(aInt, 8) + '] ['+IntToStr(aInt and $03)+':'+IntToStr(aInt shr 2)+']';
 end;
 
-{ Twb6of8EnumDef }
+{ TwbKey2Data6EnumDef }
 
-function Twb6of8EnumDef.ToSortKey(aInt: Int64; const aElement: IwbElement): string;
+function TwbKey2Data6EnumDef.ToSortKey(aInt: Int64; const aElement: IwbElement): string;
 begin
   Result := IntToHex64(aInt, 2);
 end;
 
-function Twb6of8EnumDef.ToString(aInt: Int64; const aElement: IwbElement): string;
+function TwbKey2Data6EnumDef.ToString(aInt: Int64; const aElement: IwbElement): string;
 var
   key : Integer;
   val : Integer;

TES5Edit/branches/dev-sharlikran/wbInterface.pas
@@ -860,9 +860,10 @@ type
 
     function MasterRecordsFromMasterFilesAndSelf: TDynMainRecords;
 
-    {>>> Form Version access  <<<}
     function GetFormVersion: Cardinal;
     procedure SetFormVersion(aFormVersion: Cardinal);
+
+    procedure ChangeFormSignature(aSignature: TwbSignature);
 
     property Version: Cardinal
       read GetFormVersion
@@ -3080,6 +3081,7 @@ type
     function GetIsEditable(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
     function GetEditType(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): TwbEditType; override;
     function GetEditInfo(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): string; override;
+    function SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
 
     {---IwbUnionDef---}
     function Decide(aBasePtr: Pointer; aEndPtr: Pointer; const aElement: IwbElement): IwbValueDef;
@@ -3132,6 +3134,7 @@ type
     function ToNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Variant; override;
     procedure FromNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement; const aValue: Variant); override;
     function GetIsEditable(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
+    function SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
 
     {---IwbStringDef---}
     function GetStringSize: Integer;
@@ -3206,6 +3209,7 @@ type
     function ToNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Variant; override;
     procedure FromNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement; const aValue: Variant); override;
     function GetIsEditable(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
+    function SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
   end;
 
   TwbByteArrayDef = class(TwbValueDef, IwbByteArrayDef)
@@ -3259,6 +3263,7 @@ type
     function ToNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Variant; override;
     procedure FromNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement; const aValue: Variant); override;
     function GetIsEditable(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
+    function SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
   end;
 
   TwbEmptyDef = class(TwbValueDef, IwbEmptyDef)
@@ -6445,7 +6450,7 @@ end;
 
 function TwbIntegerDef.SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean;
 begin
-  Result := ToInt(aBasePtr, aEndPtr, aElement) <> inDefault;
+  Result := not Assigned(aBasePtr) or (ToInt(aBasePtr, aEndPtr, aElement) <> inDefault);
   if Result then
     FromInt(inDefault, aBasePtr, aEndPtr, aElement);
 end;
@@ -7797,6 +7802,14 @@ begin
   Result := sdSize;
 end;
 
+function TwbStringDef.SetToDefault(aBasePtr, aEndPtr: Pointer;
+  const aElement: IwbElement): Boolean;
+begin
+  Result := not Assigned(aBasePtr) or (ToString(aBasePtr, aEndPtr, aElement) <> '');
+  if Result then
+    FromEditValue(aBasePtr, aEndPtr, aElement, '');
+end;
+
 function TwbStringDef.GetSize(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Integer;
 begin
   {if not Assigned(aBasePtr) or (Cardinal(aBasePtr) >= Cardinal(aEndPtr)) then
@@ -7964,7 +7977,7 @@ var
   Value: Extended;
 begin
   Value := ToNativeValue(aBasePtr, aEndPtr, aElement);
-  Result := not SingleSameValue(Value, fdDefault);
+  Result := not Assigned(aBasePtr) or not SingleSameValue(Value, fdDefault);
   if Result then
     FromNativeValue(aBasePtr, aEndPtr, aElement, fdDefault);
 end;
@@ -9172,6 +9185,23 @@ begin
   defReported := True;
 end;
 
+function TwbByteArrayDef.SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean;
+var
+  Size : Integer;
+  Default : String;
+begin
+  Size := GetSize(aBasePtr, aEndPtr, aElement);
+  if Size > 0  then begin
+    Default := '00';
+    while Length(Default)<(Size*3-1) do
+      Default := Default + ' 00';
+  end else
+    Default := '';
+  Result := not Assigned(aBasePtr) or (ToString(aBasePtr, aEndPtr, aElement) <> Default);
+  if Result then
+    FromEditValue(aBasePtr, aEndPtr, aElement, Default);
+end;
+
 function TwbByteArrayDef.ToEditValue(aBasePtr, aEndPtr: Pointer;
   const aElement: IwbElement): string;
 begin
@@ -10216,6 +10246,11 @@ begin
   defReported := True;
 end;
 
+function TwbUnionDef.SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean;
+begin
+  Result := Decide(aBasePtr, aEndPtr, aElement).SetToDefault(aBasePtr, aEndPtr, aElement);
+end;
+
 function TwbUnionDef.ToEditValue(aBasePtr, aEndPtr: Pointer;
   const aElement: IwbElement): string;
 begin
@@ -10556,6 +10591,14 @@ begin
       Result := Len;
   end else
     Result := Prefix;
+end;
+
+function TwbLenStringDef.SetToDefault(aBasePtr, aEndPtr: Pointer;
+  const aElement: IwbElement): Boolean;
+begin
+  Result := not Assigned(aBasePtr) or (ToString(aBasePtr, aEndPtr, aElement) <> '');
+  if Result then
+    FromEditValue(aBasePtr, aEndPtr, aElement, '');
 end;
 
 function TwbLenStringDef.ToEditValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): string;

TES5Edit/trunk/Delphi XE/wbInterface.pas
@@ -2271,7 +2271,6 @@ function GetContainerRefFromUnionOrValue(const aElement: IwbElement): IwbContain
 
 var
   HeaderSignature : TwbSignature = 'TES4';
-  
 
 implementation
 
@@ -3004,6 +3003,7 @@ type
     function GetIsEditable(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
     function GetEditType(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): TwbEditType; override;
     function GetEditInfo(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): string; override;
+    function SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
 
     {---IwbUnionDef---}
     function Decide(aBasePtr: Pointer; aEndPtr: Pointer; const aElement: IwbElement): IwbValueDef;
@@ -3056,6 +3056,7 @@ type
     function ToNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Variant; override;
     procedure FromNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement; const aValue: Variant); override;
     function GetIsEditable(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
+    function SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
 
     {---IwbStringDef---}
     function GetStringSize: Integer;
@@ -3130,6 +3131,7 @@ type
     function ToNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Variant; override;
     procedure FromNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement; const aValue: Variant); override;
     function GetIsEditable(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
+    function SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
   end;
 
   TwbByteArrayDef = class(TwbValueDef, IwbByteArrayDef)
@@ -3180,6 +3182,7 @@ type
     function ToNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Variant; override;
     procedure FromNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement; const aValue: Variant); override;
     function GetIsEditable(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
+    function SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
   end;
 
   TwbEmptyDef = class(TwbValueDef, IwbEmptyDef)
@@ -6152,7 +6155,7 @@ end;
 
 function TwbIntegerDef.SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean;
 begin
-  Result := ToInt(aBasePtr, aEndPtr, aElement) <> inDefault;
+  Result := not Assigned(aBasePtr) or (ToInt(aBasePtr, aEndPtr, aElement) <> inDefault);
   if Result then
     FromInt(inDefault, aBasePtr, aEndPtr, aElement);
 end;
@@ -7485,6 +7488,14 @@ begin
   Result := sdSize;
 end;
 
+function TwbStringDef.SetToDefault(aBasePtr, aEndPtr: Pointer;
+  const aElement: IwbElement): Boolean;
+begin
+  Result := not Assigned(aBasePtr) or (ToString(aBasePtr, aEndPtr, aElement) <> '');
+  if Result then
+    FromEditValue(aBasePtr, aEndPtr, aElement, '');
+end;
+
 function TwbStringDef.GetSize(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Integer;
 begin
   {if not Assigned(aBasePtr) or (Cardinal(aBasePtr) >= Cardinal(aEndPtr)) then
@@ -7652,7 +7663,7 @@ var
   Value: Extended;
 begin
   Value := ToNativeValue(aBasePtr, aEndPtr, aElement);
-  Result := not SingleSameValue(Value, fdDefault);
+  Result := not Assigned(aBasePtr) or not SingleSameValue(Value, fdDefault);
   if Result then
     FromNativeValue(aBasePtr, aEndPtr, aElement, fdDefault);
 end;
@@ -8854,6 +8865,23 @@ begin
   defReported := True;
 end;
 
+function TwbByteArrayDef.SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean;
+var
+  Size : Integer;
+  Default : String;
+begin
+  Size := GetSize(aBasePtr, aEndPtr, aElement);
+  if Size > 0  then begin
+    Default := '00';
+    while Length(Default)<(Size*3-1) do
+      Default := Default + ' 00';
+  end else
+    Default := '';
+  Result := not Assigned(aBasePtr) or (ToString(aBasePtr, aEndPtr, aElement) <> Default);
+  if Result then
+    FromEditValue(aBasePtr, aEndPtr, aElement, Default);
+end;
+
 function TwbByteArrayDef.ToEditValue(aBasePtr, aEndPtr: Pointer;
   const aElement: IwbElement): string;
 begin
@@ -9898,6 +9926,11 @@ begin
   defReported := True;
 end;
 
+function TwbUnionDef.SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean;
+begin
+  Result := Decide(aBasePtr, aEndPtr, aElement).SetToDefault(aBasePtr, aEndPtr, aElement);
+end;
+
 function TwbUnionDef.ToEditValue(aBasePtr, aEndPtr: Pointer;
   const aElement: IwbElement): string;
 begin
@@ -10238,6 +10271,14 @@ begin
       Result := Len;
   end else
     Result := Prefix;
+end;
+
+function TwbLenStringDef.SetToDefault(aBasePtr, aEndPtr: Pointer;
+  const aElement: IwbElement): Boolean;
+begin
+  Result := not Assigned(aBasePtr) or (ToString(aBasePtr, aEndPtr, aElement) <> '');
+  if Result then
+    FromEditValue(aBasePtr, aEndPtr, aElement, '');
 end;
 
 function TwbLenStringDef.ToEditValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): string;

TES5Edit/trunk/Delphi XE3/wbInterface.pas
@@ -2271,7 +2271,6 @@ function GetContainerRefFromUnionOrValue(const aElement: IwbElement): IwbContain
 
 var
   HeaderSignature : TwbSignature = 'TES4';
-  
 
 implementation
 
@@ -3004,6 +3003,7 @@ type
     function GetIsEditable(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
     function GetEditType(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): TwbEditType; override;
     function GetEditInfo(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): string; override;
+    function SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
 
     {---IwbUnionDef---}
     function Decide(aBasePtr: Pointer; aEndPtr: Pointer; const aElement: IwbElement): IwbValueDef;
@@ -3056,6 +3056,7 @@ type
     function ToNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Variant; override;
     procedure FromNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement; const aValue: Variant); override;
     function GetIsEditable(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
+    function SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
 
     {---IwbStringDef---}
     function GetStringSize: Integer;
@@ -3130,6 +3131,7 @@ type
     function ToNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Variant; override;
     procedure FromNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement; const aValue: Variant); override;
     function GetIsEditable(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
+    function SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
   end;
 
   TwbByteArrayDef = class(TwbValueDef, IwbByteArrayDef)
@@ -3180,6 +3182,7 @@ type
     function ToNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Variant; override;
     procedure FromNativeValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement; const aValue: Variant); override;
     function GetIsEditable(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
+    function SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean; override;
   end;
 
   TwbEmptyDef = class(TwbValueDef, IwbEmptyDef)
@@ -6152,7 +6155,7 @@ end;
 
 function TwbIntegerDef.SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean;
 begin
-  Result := ToInt(aBasePtr, aEndPtr, aElement) <> inDefault;
+  Result := not Assigned(aBasePtr) or (ToInt(aBasePtr, aEndPtr, aElement) <> inDefault);
   if Result then
     FromInt(inDefault, aBasePtr, aEndPtr, aElement);
 end;
@@ -7485,6 +7488,14 @@ begin
   Result := sdSize;
 end;
 
+function TwbStringDef.SetToDefault(aBasePtr, aEndPtr: Pointer;
+  const aElement: IwbElement): Boolean;
+begin
+  Result := not Assigned(aBasePtr) or (ToString(aBasePtr, aEndPtr, aElement) <> '');
+  if Result then
+    FromEditValue(aBasePtr, aEndPtr, aElement, '');
+end;
+
 function TwbStringDef.GetSize(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Integer;
 begin
   {if not Assigned(aBasePtr) or (Cardinal(aBasePtr) >= Cardinal(aEndPtr)) then
@@ -7652,7 +7663,7 @@ var
   Value: Extended;
 begin
   Value := ToNativeValue(aBasePtr, aEndPtr, aElement);
-  Result := not SingleSameValue(Value, fdDefault);
+  Result := not Assigned(aBasePtr) or not SingleSameValue(Value, fdDefault);
   if Result then
     FromNativeValue(aBasePtr, aEndPtr, aElement, fdDefault);
 end;
@@ -8854,6 +8865,23 @@ begin
   defReported := True;
 end;
 
+function TwbByteArrayDef.SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean;
+var
+  Size : Integer;
+  Default : String;
+begin
+  Size := GetSize(aBasePtr, aEndPtr, aElement);
+  if Size > 0  then begin
+    Default := '00';
+    while Length(Default)<(Size*3-1) do
+      Default := Default + ' 00';
+  end else
+    Default := '';
+  Result := not Assigned(aBasePtr) or (ToString(aBasePtr, aEndPtr, aElement) <> Default);
+  if Result then
+    FromEditValue(aBasePtr, aEndPtr, aElement, Default);
+end;
+
 function TwbByteArrayDef.ToEditValue(aBasePtr, aEndPtr: Pointer;
   const aElement: IwbElement): string;
 begin
@@ -9898,6 +9926,11 @@ begin
   defReported := True;
 end;
 
+function TwbUnionDef.SetToDefault(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): Boolean;
+begin
+  Result := Decide(aBasePtr, aEndPtr, aElement).SetToDefault(aBasePtr, aEndPtr, aElement);
+end;
+
 function TwbUnionDef.ToEditValue(aBasePtr, aEndPtr: Pointer;
   const aElement: IwbElement): string;
 begin
@@ -10238,6 +10271,14 @@ begin
       Result := Len;
   end else
     Result := Prefix;
+end;
+
+function TwbLenStringDef.SetToDefault(aBasePtr, aEndPtr: Pointer;
+  const aElement: IwbElement): Boolean;
+begin
+  Result := not Assigned(aBasePtr) or (ToString(aBasePtr, aEndPtr, aElement) <> '');
+  if Result then
+    FromEditValue(aBasePtr, aEndPtr, aElement, '');
 end;
 
 function TwbLenStringDef.ToEditValue(aBasePtr, aEndPtr: Pointer; const aElement: IwbElement): string;

TES5Edit/branches/dev-experimental/wbDefinitionsTES5.pas
@@ -10200,7 +10200,7 @@ begin
         wbStructExSK(LVLO , [0, 2], [3], 'Base Data', [
         wbInteger('Level', itU16),
         wbByteArray('Unknown', 2, cpIgnore, false, wbNeverShow),
-        wbFormIDCk('Reference', [SPEL]),
+        wbFormIDCk('Reference', [SPEL, LVSP]),
         wbInteger('Count', itU16),
         wbByteArray('Unknown', 2, cpIgnore, false, wbNeverShow)
       ])

TES5Edit/branches/dev-experimental/wbImplementation.pas
@@ -885,6 +885,7 @@ type
     function GetGridCell(out aGridCell: TwbGridCell): Boolean;
     function GetFormVersion: Cardinal; {>>> Form Version access <<<}
     procedure SetFormVersion(aFormVersion: Cardinal); {>>> Form Version access <<<}
+    procedure ChangeFormSignature(aSignature: TwbSignature);
 
     procedure Delete;
     procedure DeleteInto(const aFile: IwbFile);
@@ -5985,6 +5986,12 @@ procedure TwbMainRecord.SetFormVersion(aFormVersion: Cardinal);
 begin
   MakeHeaderWriteable;
   mrStruct.mrsVersion := aFormVersion;
+end;
+
+procedure TwbMainRecord.ChangeFormSignature(aSignature: TwbSignature);
+begin
+  MakeHeaderWriteable;
+  mrStruct.mrsSignature := aSignature;
 end;
 
 function TwbMainRecord.GetGridCell(out aGridCell: TwbGridCell): Boolean;

TES5Edit/branches/dev-experimental/wbScriptAdapter.pas
@@ -691,6 +691,17 @@ begin
     Value := MainRecord.CompareExchangeFormID(Cardinal(Args.Values[1]), Cardinal(Args.Values[2]));
 end;
 
+procedure IwbMainRecord_ChangeFormSignature(var Value: Variant; Args: TJvInterpreterArgs);
+var
+  MainRecord: IwbMainRecord;
+  Signature: TwbSignature;
+begin
+  if Supports(IInterface(Args.Values[0]), IwbMainRecord, MainRecord) then begin
+    Signature := StrToSignature(Args.Values[1]);
+    MainRecord.ChangeFormSignature(Signature);
+  end;
+end;
+
 
 { IwbGroupRecord }
 
@@ -1036,6 +1047,7 @@ begin
     AddFunction(cUnit, 'WinningOverride', IwbMainRecord_WinningOverride, 1, [varEmpty], varEmpty);
     AddFunction(cUnit, 'ChildGroup', IwbMainRecord_ChildGroup, 1, [varEmpty], varEmpty);
     AddFunction(cUnit, 'CompareExchangeFormID', IwbMainRecord_CompareExchangeFormID, 3, [varEmpty, varEmpty, varEmpty], varEmpty);
+    AddFunction(cUnit, 'ChangeFormSignature', IwbMainRecord_ChangeFormSignature, 2, [varEmpty, varEmpty], varEmpty);
 
     { IwbGroupRecord }
     AddFunction(cUnit, 'GroupType', IwbGroupRecord_GroupType, 1, [varEmpty], varEmpty);

TES5Edit/branches/dev-sharlikran/wbDefinitionsTES5.pas
@@ -10200,7 +10200,7 @@ begin
         wbStructExSK(LVLO , [0, 2], [3], 'Base Data', [
         wbInteger('Level', itU16),
         wbByteArray('Unknown', 2, cpIgnore, false, wbNeverShow),
-        wbFormIDCk('Reference', [SPEL]),
+        wbFormIDCk('Reference', [SPEL, LVSP]),
         wbInteger('Count', itU16),
         wbByteArray('Unknown', 2, cpIgnore, false, wbNeverShow)
       ])

commit 4ef1e3722ee0cab09c63ceba0824690e79ae3635

[All] Fix issue introduced by r1259 for Byte Arrays
git-svn-id: http://skyrim-plugin-decoding-project.googlecode.com/svn@1260 bdfc8fb3-84cf-a68a-e902-ed0415cf5604

TES5Edit/branches/dev-experimental/wbInterface.pas
@@ -8972,7 +8972,7 @@ var
   Default : String;
 begin
   Size := GetSize(aBasePtr, aEndPtr, aElement);
-  if Size > 0  then begin
+  if (Size > 0) and (Size < High(Integer))  then begin
     Default := '00';
     while Length(Default)<(Size*3-1) do
       Default := Default + ' 00';

TES5Edit/branches/dev-sharlikran/wbInterface.pas
@@ -9191,7 +9191,7 @@ var
   Default : String;
 begin
   Size := GetSize(aBasePtr, aEndPtr, aElement);
-  if Size > 0  then begin
+  if (Size > 0) and (Size < High(Integer))  then begin
     Default := '00';
     while Length(Default)<(Size*3-1) do
       Default := Default + ' 00';

TES5Edit/trunk/Delphi XE/wbInterface.pas
@@ -8871,7 +8871,7 @@ var
   Default : String;
 begin
   Size := GetSize(aBasePtr, aEndPtr, aElement);
-  if Size > 0  then begin
+  if (Size > 0) and (Size < High(Integer))  then begin
     Default := '00';
     while Length(Default)<(Size*3-1) do
       Default := Default + ' 00';

TES5Edit/trunk/Delphi XE3/wbInterface.pas
@@ -8871,7 +8871,7 @@ var
   Default : String;
 begin
   Size := GetSize(aBasePtr, aEndPtr, aElement);
-  if Size > 0  then begin
+  if (Size > 0) and (Size < High(Integer))  then begin
     Default := '00';
     while Length(Default)<(Size*3-1) do
       Default := Default + ' 00';